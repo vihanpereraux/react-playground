b167c176ff74b690ba5093d89daa72bb6597e9fa
Vihan Perera <vihanpereraux@gmail.com>
2024-03-31 08:39:50 +0100

    Update navbar comp

--- mern-todo-app/client/src/pages/Home.tsx
+++ mern-todo-app/client/src/pages/Home.tsx
@@ -1,67 +1,47 @@
 import React, { useState } from "react";
-import { Container, Typography, Box } from '@mui/material';
+import { Container, Typography, Box } from "@mui/material";
 
 // components
-import Create from '../components/Create';
-import NavBar from '../components/Nav';
+import Create from "../components/Create";
+import NavBar from "../components/Nav";
 
 function Home() {
-    const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-    return (
-        <>
-            <Container
-                sx={{
-                    textAlign: 'center',
-                    background: 'none',
-                    color: 'white'
-                }}>
-                <NavBar />
-            </Container>
-            <Container
-                fixed
-                sx={{
-                    textAlign: 'center',
-                    background: 'none',
-                    pt: 3,
-                    pb: 3
-                }}>
-                {/* heading */}
-                <Typography sx={{
-                    color: 'white',
-                    fontSize: 25
-                }}>
-                    Dummy Dev Test List</Typography>
+  return (
+    <>
+      <Container
+        sx={{ textAlign: "center", background: "none", color: "white" }}
+      >
+        <NavBar />
+      </Container>
+      <Container
+        fixed
+        sx={{ textAlign: "center", background: "none", pt: 3, pb: 3 }}
+      >
+        {/* heading */}
+        <Typography sx={{ color: "white", fontSize: 25 }}>
+          Dummy Dev Test List
+        </Typography>
 
-                {/* form comp */}
-                <Box sx={{
-                    mt: 4,
-                    mb: 2
-                }}>
-                    <Create />
-                </Box>
+        {/* form comp */}
+        <Box sx={{ mt: 10, mb: 2 }}>
+          <Create />
+        </Box>
 
-                {/* rending the fetched list */}
-                <Box sx={{
-                    mt: 5,
-                }}>
-                    {
-                        todos.length === 0 ?
-                            <Typography sx={{
-                                color: 'white',
-                                fontSize: 18
-                            }}>
-                                No items found
-                            </Typography>
-                            :
-                            todos.map(todo => (
-                                <div>{todo}</div>
-                            ))
-                    }
-                </Box>
-            </Container>
-        </>
-    );
+        {/* rending the fetched list */}
+        <Box sx={{ mt: 5 }}>
+          {todos.length === 0 ? (
+            <Typography sx={{ color: "white", fontSize: 18 }}>
+              No items found
+            </Typography>
+          ) : (
+            todos.map((todo) => <div key={todo}>{todo}</div>)
+          )}
+        </Box>
+      </Container>
+    </>
+  );
 }
 
-export default Home
+export default Home;
